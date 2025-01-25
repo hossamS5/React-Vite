d92f9b47aac709da192d2cd34e20b60248612780
hossam <hossam.salem667@gmail.com>
2025-01-25 19:21:24 +0200

    husky

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
+  console.log('hossam');
+
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
